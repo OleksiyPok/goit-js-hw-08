b6b12c650c9b203b77e3aeb717bba8e60352ea7b
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-02-12 11:48:36 +0100

    added task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_PLAYER_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+onResumePlayer();
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(data) {
+  localStorage.setItem(CURRENT_PLAYER_TIME_KEY, data.seconds);
+}
+
+function onResumePlayer() {
+  const currentPlayerTime = localStorage.getItem(CURRENT_PLAYER_TIME_KEY);
+  if (currentPlayerTime) player.setCurrentTime(currentPlayerTime);
+}
